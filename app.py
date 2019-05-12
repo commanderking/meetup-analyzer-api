c2f461a2562e7e4c925588972ff9fa26292d0835
commanderking <jek582@mail.harvard.edu>
2019-05-12 16:00:58 -0400

    add first endpoint

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+import json
+import requests
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Might need to reference here for heroku deployment: https://realpython.com/flask-by-example-part-1-project-setup/
+# Might need to reference here for heroku deployment: https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
 
 # Start up notes: 
 # export APP_SETTINGS="config.DevelopmentConfig"
@@ -16,6 +19,14 @@ db = SQLAlchemy(app)
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/events', methods=['GET', 'POST'])
+def events():
+    my_data = json.loads(request.data.decode('utf-8'))
+    print(my_data['name'])
+    print(request.method)
+    return jsonify(my_data)
+
 
 if __name__ == '__main__':
     app.run()
