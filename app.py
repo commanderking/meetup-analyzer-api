2047326187c549ea346cf33c4284992c479c0972
commanderking <jek582@mail.harvard.edu>
2019-05-27 14:25:19 -0400

    endpoint to get all event data

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from models import Events, Attendance
+from extensions import db
 from flask import Flask, request, jsonify, _request_ctx_stack
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -9,21 +11,23 @@ from flask_cors import cross_origin
 from jose import jwt
 
 AUTH0_DOMAIN = os.environ["AUTH0_DOMAIN"]
-API_AUDIENCE= os.environ["API_AUDIENCE"]
+API_AUDIENCE = os.environ["API_AUDIENCE"]
 ALGORITHMS = ["RS256"]
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from extensions import db
 db.init_app(app)
 
 # Error handler
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
 
 @app.errorhandler(AuthError)
 def handle_auth_error(ex):
@@ -40,30 +44,33 @@ def get_token_auth_header():
     """
 
     auth = request.headers.get("Authorization", None)
+    print('auth')
+    print(auth)
     if not auth:
         raise AuthError({"code": "authorization_header_missing",
-                        "description":
-                            "Authorization header is expected"}, 401)
+                         "description":
+                         "Authorization header is expected"}, 401)
 
     parts = auth.split()
 
     if parts[0].lower() != "bearer":
         raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must start with"
-                            " Bearer"}, 401)
+                         "description":
+                         "Authorization header must start with"
+                         " Bearer"}, 401)
     elif len(parts) == 1:
         raise AuthError({"code": "invalid_header",
-                        "description": "Token not found"}, 401)
+                         "description": "Token not found"}, 401)
     elif len(parts) > 2:
         raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must be"
-                            " Bearer token"}, 401)
+                         "description":
+                         "Authorization header must be"
+                         " Bearer token"}, 401)
 
     token = parts[1]
 
     return token
+
 
 def requires_auth(f):
     """Determines if the Access Token is valid
@@ -71,7 +78,8 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = get_token_auth_header()
-        jsonurl = requests.get("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json").json()
+        jsonurl = requests.get("https://"+AUTH0_DOMAIN +
+                               "/.well-known/jwks.json").json()
         jwks = json.loads(json.dumps(jsonurl))
         unverified_header = jwt.get_unverified_header(token)
         rsa_key = {}
@@ -95,31 +103,31 @@ def requires_auth(f):
                 )
             except jwt.ExpiredSignatureError:
                 raise AuthError({"code": "token_expired",
-                                "description": "token is expired"}, 401)
+                                 "description": "token is expired"}, 401)
             except jwt.JWTClaimsError:
                 raise AuthError({"code": "invalid_claims",
-                                "description":
-                                    "incorrect claims,"
-                                    "please check the audience and issuer"}, 401)
+                                 "description":
+                                 "incorrect claims,"
+                                 "please check the audience and issuer"}, 401)
             except Exception:
                 raise AuthError({"code": "invalid_header",
-                                "description":
-                                    "Unable to parse authentication"
-                                    " token."}, 401)
+                                 "description":
+                                 "Unable to parse authentication"
+                                 " token."}, 401)
 
             _request_ctx_stack.top.current_user = payload
             return f(*args, **kwargs)
         raise AuthError({"code": "invalid_header",
-                        "description": "Unable to find appropriate key"}, 401)
+                         "description": "Unable to find appropriate key"}, 401)
     return decorated
 
 # Might need to reference here for heroku deployment: https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
 
-from models import Events, Attendance
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
 
 @app.route('/login', methods=['GET', 'POST'])
 @cross_origin(headers=['Content-Type', 'Authorization'])
@@ -131,40 +139,50 @@ def login():
 
 @app.route('/events', methods=['GET', 'POST'])
 def events():
-    my_data = json.loads(request.data.decode('utf-8'))
-    name = my_data['eventDate']
-    print(name)
-    event = Events(
-        event_name = my_data['eventName'],
-        event_date = my_data['eventDate']
-    )
-    
-    try:
-        db.session.add(event)
 
-        # Need to commit event first so I can use the event_id to link two tables
-        db.session.commit()
-        attendees = []
-        for attendee in my_data['attendees']:
-            attendees.append(Attendance(
-                meetup_user_id = attendee['userId'],
-                event_id = event.id,
-                did_attend = attendee['didAttend'],
-                did_rsvp = attendee['didRSVP'],
-                title = attendee['title'],
-                event_host = attendee['eventHost'],
-                rsvp_date = attendee['rsvpDate'],
-                date_joined_group = attendee['dateJoinedGroup']
-            ))
-        print(attendees)
-        print(event)
-        db.session.bulk_save_objects(attendees)
-        db.session.commit()
+    if request.method == "POST":
+        my_data = json.loads(request.data.decode('utf-8'))
+        name = my_data['eventDate']
+        print(name)
+        event = Events(
+            event_name=my_data['eventName'],
+            event_date=my_data['eventDate']
+        )
 
-        
-    except Exception as exception: 
-        print(exception)
-    return jsonify(my_data)
+        try:
+            db.session.add(event)
+
+            # Need to commit event first so I can use the event_id to link two tables
+            db.session.commit()
+            attendees = []
+            for attendee in my_data['attendees']:
+                attendees.append(Attendance(
+                    meetup_user_id=attendee['userId'],
+                    event_id=event.id,
+                    did_attend=attendee['didAttend'],
+                    did_rsvp=attendee['didRSVP'],
+                    title=attendee['title'],
+                    event_host=attendee['eventHost'],
+                    rsvp_date=attendee['rsvpDate'],
+                    date_joined_group=attendee['dateJoinedGroup']
+                ))
+            print(attendees)
+            print(event)
+            db.session.bulk_save_objects(attendees)
+            db.session.commit()
+
+        except Exception as exception:
+            print(exception)
+        return jsonify(my_data)
+    else:
+        events = []
+        for event in Events.query.all():
+            events.append({
+                "id": event.id,
+                "event_name": event.event_name,
+                "event_date": event.event_date
+            })
+        return jsonify(data=events)
 
 
 if __name__ == '__main__':
