2f8f9531e04babd2fcac53127df76065f608ceb1
commanderking <jek582@mail.harvard.edu>
2019-08-31 22:02:31 -0400

    add button rather than have entire card be clickable

--- web/src/features/dashboard/components/EventCard.tsx
+++ web/src/features/dashboard/components/EventCard.tsx
@@ -7,6 +7,8 @@ import { EventResponse } from "../../../requests/eventTypes";
 import EventCardStat from "./EventCardStat";
 // @ts-ignore
 import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+
 type Props = {
   event: EventResponse;
 };
@@ -16,50 +18,57 @@ const EventCard = ({ event }: Props) => {
   const attendancePercent = `${Math.round((attendeesWhoRsvpd / rsvps) * 100)}%`;
 
   return (
-    <Link to={`/event/${id}`}>
-      <button
-        css={css`
+    <Card
+      css={css`
          {
-          border: none;
-          padding 0;
+          padding: 25px;
           height: 100%;
-          width: 100%;
         }
       `}
+    >
+      <div
+        css={css`
+           {
+            min-height: 100px;
+          }
+        `}
       >
-        <Card
-          css={css`
-             {
-              padding: 25px;
-              height: 100%;
+        <h5>{name}</h5>
+        <h6>{moment(date).format("MM/DD/YY")}</h6>
+      </div>
+      <div
+        css={css`
+           {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            margin-bottom: 30px;
+          }
+        `}
+      >
+        <EventCardStat stat={attendees} description="Attendees" />
+        <EventCardStat stat={attendancePercent} description="Show Rate" />
+      </div>
+      <Link
+        to={`/event/${id}`}
+        css={css`
+           {
+            text-decoration: none;
+            &:focus,
+            &:hover,
+            &:visited,
+            &:link,
+            &:active {
+              text-decoration: none;
             }
-          `}
-        >
-          <div
-            css={css`
-               {
-                min-height: 100px;
-              }
-            `}
-          >
-            <h5>{name}</h5>
-            <h6>{moment(date).format("MM/DD/YY")}</h6>
-          </div>
-          <div
-            css={css`
-               {
-                display: flex;
-                flex-direction: row;
-                justify-content: center;
-              }
-            `}
-          >
-            <EventCardStat stat={attendees} description="Attendees" />
-            <EventCardStat stat={attendancePercent} description="Show Rate" />
-          </div>
-        </Card>
-      </button>
-    </Link>
+          }
+        `}
+      >
+        <Button color="primary" variant="outlined">
+          Details
+        </Button>
+      </Link>
+    </Card>
   );
 };
 
