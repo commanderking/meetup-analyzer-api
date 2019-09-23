6ac0453952a61849dd007256474eea7f975ae960
commanderking <jek582@mail.harvard.edu>
2019-09-23 08:02:37 -0400

    remove auth related python code

--- app.py
+++ app.py
@@ -11,14 +11,9 @@ from datetime import datetime
 
 from functools import wraps
 from flask_cors import cross_origin
-from jose import jwt
 from flask_cors import CORS
 
 from eventPrediction.eventPredictionUtils import getLinearRegressionPrediction, getAttendanceRateForThoseAttendingSingleMeetup, getPredictedAttendeesOfMembers
-
-AUTH0_DOMAIN = os.environ["AUTH0_DOMAIN"]
-API_AUDIENCE = os.environ["API_AUDIENCE"]
-ALGORITHMS = ["RS256"]
 
 app = Flask(__name__, static_folder="web/build/static",
             template_folder="web/build")
@@ -28,122 +23,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 CORS(app)
 
-# Error handler
-
-
-class AuthError(Exception):
-    def __init__(self, error, status_code):
-        self.error = error
-        self.status_code = status_code
-
-
-@app.errorhandler(AuthError)
-def handle_auth_error(ex):
-    response = jsonify(ex.error)
-    response.status_code = ex.status_code
-    return response
-
-
-# /server.py
-
-# Format error response and append status code
-def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-    """
-
-    auth = request.headers.get("Authorization", None)
-    print('auth')
-    print(auth)
-    if not auth:
-        raise AuthError({"code": "authorization_header_missing",
-                         "description":
-                         "Authorization header is expected"}, 401)
-
-    parts = auth.split()
-
-    if parts[0].lower() != "bearer":
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                         "Authorization header must start with"
-                         " Bearer"}, 401)
-    elif len(parts) == 1:
-        raise AuthError({"code": "invalid_header",
-                         "description": "Token not found"}, 401)
-    elif len(parts) > 2:
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                         "Authorization header must be"
-                         " Bearer token"}, 401)
-
-    token = parts[1]
-
-    return token
-
-
-def requires_auth(f):
-    """Determines if the Access Token is valid
-    """
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = get_token_auth_header()
-        jsonurl = requests.get("https://"+AUTH0_DOMAIN +
-                               "/.well-known/jwks.json").json()
-        jwks = json.loads(json.dumps(jsonurl))
-        unverified_header = jwt.get_unverified_header(token)
-        rsa_key = {}
-        for key in jwks["keys"]:
-            if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
-        if rsa_key:
-            try:
-                payload = jwt.decode(
-                    token,
-                    rsa_key,
-                    algorithms=ALGORITHMS,
-                    audience=API_AUDIENCE,
-                    issuer="https://"+AUTH0_DOMAIN+"/"
-                )
-            except jwt.ExpiredSignatureError:
-                raise AuthError({"code": "token_expired",
-                                 "description": "token is expired"}, 401)
-            except jwt.JWTClaimsError:
-                raise AuthError({"code": "invalid_claims",
-                                 "description":
-                                 "incorrect claims,"
-                                 "please check the audience and issuer"}, 401)
-            except Exception:
-                raise AuthError({"code": "invalid_header",
-                                 "description":
-                                 "Unable to parse authentication"
-                                 " token."}, 401)
-
-            _request_ctx_stack.top.current_user = payload
-            return f(*args, **kwargs)
-        raise AuthError({"code": "invalid_header",
-                         "description": "Unable to find appropriate key"}, 401)
-    return decorated
-
 # Might need to reference here for heroku deployment: https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
-
-
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def single_page_app(path):
     return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-@cross_origin(headers=['Content-Type', 'Authorization'])
-@requires_auth
-def login():
-    response = "Hello from a private endpoint! You need to be authenticated to see this."
-    return jsonify(message=response)
 
 
 @app.route('/events', methods=['GET', 'POST'])
